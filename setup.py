b043c23c59acb406ab7ed69196066fc45b930281
yangshaohua <yangshh@szreach.comm>
2017-07-20 15:16:28 +0800

    增加setup.py文件使用包安装

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='Makeup',
+      version='1.0',
+      description='A html parse',
+      author='reach',
+      py_modules=['parse', 'handlers.handlers', 'rules.rules', 'util.util'])
+# import py2exe
+# setup(
+#       console=['parse.py']
+# )
+
